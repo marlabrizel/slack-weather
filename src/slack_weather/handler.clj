fa56adf37e19f6a8d3fd6002423b28f9b4c00977
Marla Brizel <marlabrizel@gmail.com>
2016-09-02 14:42:23 -0600

    the most basic of slack integrations

--- src/slack_weather/handler.clj
+++ src/slack_weather/handler.clj
@@ -1,11 +1,21 @@
 (ns slack-weather.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clj-http.client :as client]
+            [clojure.data.json :as json]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
+
+(def hook-url
+  "https://hooks.slack.com/services/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")
+
+(defn post-to-slack
+  [url msg]
+  (client/post url {:body (json/write-str msg)
+                    :content-type :json}))
 
 (def app
   (wrap-defaults app-routes site-defaults))
